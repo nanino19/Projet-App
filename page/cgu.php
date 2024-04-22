222de272c73b5a6c9e13a25e41da15c2028bd557
alexisssmrt <alexis.martin@eleve.isep.fr>
2024-04-22 13:55:51 +0200

    alexis 2

--- page/cgu.php
+++ page/cgu.php
@@ -1,28 +1,44 @@
 <?php include('../composant/header.php'); ?>
 <?php include('../composant/menu.php'); ?>
 
-<div class="container">
-    <h5> Conditions Générales d'Utilisation
+<div class="container2">
+    <h4> Conditions Générales d'Utilisation</h4>
 
-Bienvenue sur le site web du cinéma [Nom du Cinéma]. Les présentes Conditions Générales d'Utilisation (CGU) régissent votre utilisation de ce site. En accédant à ce site et en l'utilisant, vous acceptez d'être lié par ces CGU. Si vous n'acceptez pas ces conditions, veuillez ne pas utiliser ce site.
+    <h5> Bienvenue sur le site web du cinéma Cinemanager. Les présentes Conditions Générales d'Utilisation (CGU) régissent votre utilisation
+    de ce site. En accédant à ce site et en l'utilisant, vous acceptez d'être lié par ces CGU. Si vous n'acceptez pas ces conditions, 
+    veuillez ne pas utiliser ce site. </h5>
 
-Utilisation du Site
-Vous acceptez d'utiliser ce site uniquement à des fins légales et conformément à ces CGU. Vous acceptez de ne pas utiliser ce site de manière frauduleuse, abusive ou contraire à la loi.
+    <h6> Utilisation du Site </h6>
 
-Propriété Intellectuelle
-Tous les contenus présents sur ce site, y compris mais sans s'y limiter, les textes, images, logos, marques et vidéos, sont la propriété exclusive du cinéma [Nom du Cinéma] ou de ses concédants de licence, et sont protégés par les lois sur la propriété intellectuelle.
+    <h5> Vous acceptez d'utiliser ce site uniquement à des fins légales et conformément à ces CGU. Vous acceptez de ne pas utiliser ce site de
+    manière frauduleuse, abusive ou contraire à la loi. </h5>
 
-Confidentialité
-Veuillez consulter notre Politique de Confidentialité pour comprendre comment nous recueillons, utilisons et protégeons vos informations personnelles.
+    <h6> Propriété Intellectuelle </h6>
 
-Liens vers des Sites Tiers
-Ce site peut contenir des liens vers des sites web tiers qui ne sont pas contrôlés par le cinéma [Nom du Cinéma]. Nous déclinons toute responsabilité quant au contenu de ces sites tiers et aux pratiques en matière de confidentialité.
+    <h5> Tous les contenus présents sur ce site, y compris mais sans s'y limiter, les textes, images, logos, marques et vidéos, 
+    sont la propriété exclusive du cinéma [Nom du Cinéma] ou de ses concédants de licence, et sont protégés par les lois sur la 
+    propriété intellectuelle. </h5>
 
-Modifications des CGU
-Le cinéma [Nom du Cinéma] se réserve le droit de modifier ces CGU à tout moment et sans préavis. Nous vous encourageons à consulter régulièrement cette page pour rester informé des éventuelles modifications.
+    <h6> Confidentialité </h6>
 
-En utilisant ce site, vous acceptez ces Conditions Générales d'Utilisation. Si vous avez des questions ou des préoccupations concernant ces CGU, veuillez nous contacter à [adresse e-mail du cinéma].
-Dernière mise à jour : [Date de la dernière mise à jour des CGU] </h5>
+    <h5> Veuillez consulter notre Politique de Confidentialité pour comprendre comment nous recueillons, utilisons et protégeons vos 
+    informations personnelles. </h5>
+
+    <h6> Liens vers des Sites Tiers </h6>
+
+    <h5> Ce site peut contenir des liens vers des sites web tiers qui ne sont pas contrôlés par le cinéma [Nom du Cinéma]. Nous déclinons toute 
+    responsabilité quant au contenu de ces sites tiers et aux pratiques en matière de confidentialité. </h5>
+
+    <h6> Modifications des CGU </h6>
+
+    <h5> Le cinéma [Nom du Cinéma] se réserve le droit de modifier ces CGU à tout moment et sans préavis. Nous vous encourageons à 
+    consulter régulièrement cette page pour rester informé des éventuelles modifications. </h5>
+
+    <h5> En utilisant ce site, vous acceptez ces Conditions Générales d'Utilisation. Si vous avez des questions ou des préoccupations 
+    concernant ces CGU, veuillez nous contacter à [adresse e-mail du cinéma]. </h5>
+
+    <h7> Dernière mise à jour : [Date de la dernière mise à jour des CGU] </h7>
+</div>
 
 
 
