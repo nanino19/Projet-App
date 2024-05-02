55522b5ba714b8dbda8bd88bddb8ccc05e8ace60
Tim-lfrt <tile61820@eleve.isep.fr>
2024-05-02 09:51:25 +0200

    réparation2

--- composant/menu.php
+++ composant/menu.php
@@ -1,22 +1,20 @@
 <?php
-
 session_start();
 
 if (isset($_SESSION['user']) && $_SESSION['user']['role'] == 1) {
 	include("aside-admin.php");
 }
-
 echo '
 <div class= "header">
 	<a href="../index.php">
 	<img src="../image/logo.png" alt="Logo" class="logo" >
-</a>
+	</a>
 	<nav>
 		<ul class="menu">
 			<li><a href="../page/Nousdecouvrir.php">Nous decouvrir</a></li>
-			<li><a href="../page/nosfilms.php">Films</a></li>
+			<li><a href="../page/films_affiche.php">Films</a></li>
 			<li><a href="../page/faq.php">Forum</a></li>
-			<li><a href="../page/connexion.php">Se connecter</a></li>
+			<li><a href="../page/connexion.php">Mon Compte</a></li>
 			<li><a href="../page/inscription.php">Creer un Compte</a></li>
 		</ul>
 	</nav>
